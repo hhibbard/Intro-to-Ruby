fa0b2dd85175c11eb13ab8aeadab9306f6d9aaca
hhibbard <68026145+hhibbard@users.noreply.github.com>
2020-10-08 13:33:09 -0400

    Add The Basics - Exercise 2

--- exercise_two.rb
+++ exercise_two.rb
@@ -2,3 +2,8 @@ thousands = 5823 / 1000
 hundreds = 5823 % 1000 / 100
 tens = 5823 % 100 / 10
 ones = 5823 % 10
+
+puts "thousands: #{thousands}"
+puts "hundreds: #{hundreds}"
+puts "tens: #{tens}"
+puts "ones: #{ones}"
