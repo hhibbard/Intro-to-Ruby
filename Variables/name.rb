55b146729f8ce0fdaa76c411efbf6b1b3a92a22b
hhibbard <68026145+hhibbard@users.noreply.github.com>
2020-10-09 09:46:50 -0400

    Add Variables - Exercise 4

--- Variables/name.rb
+++ Variables/name.rb
@@ -1,6 +1,8 @@
 puts "What's your first name?"
 first_name = gets.chomp
-puts "Hello, #{first_name}!"
+puts "What's your last name?"
+last_name = gets.chomp
+puts "Hello, #{first_name}" + " " + "#{last_name}!"
 
-#name.rb continued
-10.times{ puts first_name}
+#name.rb continued - removed for exercise 4
+#10.times{ puts first_name}
